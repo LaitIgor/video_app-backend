074c4c1db6aef541eddc402d2426a381642e4ad3
Lait Igor <laitigorofficial@gmail.com>
2024-02-17 15:46:49 +0100

    add video routes and controllers

--- controllers/user.js
+++ controllers/user.js
@@ -18,22 +18,72 @@ export const updateUser = async (req, res, next) => {
     }
 }
 
-export const deleteUser = (req, res, next) => {
+export const deleteUser = async (req, res, next) => {
+    if(req.params.id === req.user.id) {
+        try {
+            await User.findByIdAndDelete(req.user.id);
+            res.status(200).json('User has been deleted');
+
+        } catch(err) {
+            next(err);
+        }
+    } else {
+        return next(createError(403, 'You can delete only your account'))
+    }
 }
 
-export const getUser = (req, res, next) => {
+export const getUser = async (req, res, next) => {
+    try {
+        const user = await User.findByI(req.params.id);
+        res.status(200).json(user);
+    } catch(err) {
+        next(err);
+    }
+
 }
 
-export const subscribe = (req, res, next) => {
+export const subscribe = async (req, res, next) => {
+    try {
+        await User.findByIdAndUpdate(req.user.id, {
+            $push: { subscribedUsers: req.params.id}
+        })
+        await User.findByIdAndUpdate(req.params.id, {
+            $inc: { subscribers: 1 },
+        })
+        res.status(200).json("Subscription successful")
+    } catch(err) {
+        next(err);
+    }
 }
 
-export const unsubscribe = (req, res, next) => {
+export const unsubscribe = async (req, res, next) => {
+    try {
+        await User.findByIdAndUpdate(req.user.id, {
+            $pull: { subscribedUsers: req.params.id}
+        })
+        await User.findByIdAndUpdate(req.params.id, {
+            $inc: { subscribers: -1 },
+        })
+        res.status(200).json("Usubscription successful")
+    } catch(err) {
+        next(err);
+    }
 }
 
-export const like = (req, res, next) => {
+export const like = async (req, res, next) => {
+    try {
+        
+    } catch(err) {
+        next(err);
+    }
 }
 
-export const dislike = (req, res, next) => {
+export const dislike = async (req, res, next) => {
+    try {
+        
+    } catch(err) {
+        next(err);
+    }
 }
 
 // {
