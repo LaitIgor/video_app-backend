712c6bcc1d1e79c25a6fd72f07e796f7e3bc0ebe
Lait Igor <laitigorofficial@gmail.com>
2024-02-17 19:07:53 +0100

    add comment routes and controllers

--- routes/videos.js
+++ routes/videos.js
@@ -1,5 +1,16 @@
 import express from 'express';
-import { addVideo, addView, deleteVideo, getVideo, random, sub, trend, updateVideo } from '../controllers/video.js';
+import { 
+    addVideo, 
+    addView, 
+    deleteVideo, 
+    getByTag, 
+    getVideo, 
+    random, 
+    search, 
+    sub, 
+    trend, 
+    updateVideo 
+} from '../controllers/video.js';
 import { verifyToken } from '../verifyToken.js';
 
 const router = express.Router();
@@ -13,5 +24,7 @@ router.put("/view/:id", addView )
 router.get("/trend/:id", trend )
 router.get("/random/:id", random )
 router.get("/subscibed/:id", verifyToken, sub )
+router.get("/tags", getByTag )
+router.get("/search", search )
 
 export default router;
